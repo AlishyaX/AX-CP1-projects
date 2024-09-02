a8370d60aeb459f9dbb8eddbb98f43d3046db268
AlishyaX <alishya.xavier@ucas-edu.net>
2024-09-01 20:20:56 -0600

    Made my input project

---
+++ whoareyou/main.py
@@ -0,0 +1,8 @@
+#Alishya Xavier
+#who are you?
+
+name = input("What is your name:")
+age = input("What is your age:")
+fav_color = input("What is your favorite color:")
+
+print('My name is',name,'and I am',age,'years old, My favorie color is',fav_color,'.')
