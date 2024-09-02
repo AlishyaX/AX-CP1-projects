2e5feee5a4efe054fd5893dd0d81826cefb67c2a
AlishyaX <alishya.xavier@ucas-edu.net>
2024-09-02 17:45:35 -0600

    fixed spelling error

--- whoareyou/main.py
+++ whoareyou/main.py
@@ -5,4 +5,4 @@ name = input("What is your name:")
 age = input("What is your age:")
 fav_color = input("What is your favorite color:")
 
-print('My name is',name,'and I am',age,'years old, My favorie color is',fav_color,'.')
+print('My name is',name,'and I am',age,'years old also my favorie color is',fav_color,'.')
