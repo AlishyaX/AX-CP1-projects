d2ce70ef035a43f257933b61a3f0bdf2c1318d4c
AlishyaX <alishya.xavier@ucas-edu.net>
2024-10-24 10:10:55 -0600

    skill practice

---
+++ Conditionals/Skill practice.py
@@ -0,0 +1,20 @@
+#Alishya Xavier, Skill practice what is my grade
+
+#Alishya Xavier, What is my grade SkillPractice
+
+
+user_grade = float(input('What is your grade percentage?'))
+
+
+if user_grade >= 90:
+    print('You have an A')
+elif user_grade < 90 and user_grade >= 80:
+    print('You have a B')
+elif user_grade < 80 and user_grade >= 70:
+    print('You have a C')
+elif user_grade < 70 and user_grade >= 60:
+    print('You have a D')
+else:
+    print('You have an F')
+
+
