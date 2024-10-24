9c93edb714cc3d715270fc7d7b94f41d4d194471
AlishyaX <alishya.xavier@ucas-edu.net>
2024-10-24 10:46:46 -0600

    started profieciency test what are these nums

---
+++ Formatting/Profiecency test.py
@@ -0,0 +1,10 @@
+#Alishya Xavier, Profiecency test what are these numbers
+'''
+num_int = int(input('Type in a number:'))
+
+txt = "{:,}"
+print(txt.format(num_int))
+'''
+num_float = float(input('Type in a number:'))
+
+txt2 = "{:}"
