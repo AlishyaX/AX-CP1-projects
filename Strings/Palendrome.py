c8f9389b3f85d99fd8ea005c9379d1b8959edd73
AlishyaX <alishya.xavier@ucas-edu.net>
2024-10-28 10:43:51 -0600

    made palindrome

---
+++ Strings/Palendrome.py
@@ -0,0 +1,11 @@
+#Alishya Xavier, Skill practice Palendrome
+
+user = input('Type in your word: ')
+
+if user == (user[::-1]):
+    print('That is a Palindrome.')
+else:
+    print('That is not a Palindrome')
+
+
+
