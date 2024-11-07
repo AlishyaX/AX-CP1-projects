7005600c452d55e6d0aed700bd6e349c7c3cf8d8
AlishyaX <alishya.xavier@ucas-edu.net>
2024-11-07 10:50:42 -0700

    Started profiecency test

---
+++ While loops/Proficiency Test.py
@@ -0,0 +1,5 @@
+#Alishya Xavier, Profiecency test: Rock, Paper, Scissors
+score = 0
+while True:
+    user = input('Type in your choice(rock, paper, or scissors): ')
+    
--- While loops/Skill practice.py
+++ While loops/Skill practice.py
@@ -13,10 +13,12 @@ while True:
         print('Your password passed the special character')  
     else:
         print('Your password doesn\'t have at least one special character.')
+        continue
     if '1' in password or '3' in password or '4' in password or '5' in password or '6' in password or '7' in password or '8' in password or '9' in password:
         print('Your password passed the at least one number check')
     else:
         print('Your password didn\'t pass the at least one number check.')
+        continue
     if len(password) >= 8 and '@' in password or '#' in password or '$' in password or '&' in password or '*' in password and '1' in password or '3' in password or '4' in password or '5' in password or '6' in password or '7' in password or '8' in password or '9' in password:
         True
         print('Your password is strong enough!')
