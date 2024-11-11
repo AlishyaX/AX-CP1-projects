3fc7ba1a30ab7a9e5aaa1dc57fdbef46b073a26b
AlishyaX <alishya.xavier@ucas-edu.net>
2024-11-11 10:50:24 -0700

    started secret Cypher

---
+++ Functions/ProficiencyTest.py
@@ -0,0 +1,17 @@
+#Alishya Xavier, ProficiencyTest: Secret Cypher
+
+def cypher():
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    new_alphabet = ""
+    code = str(input("Type in the code that you want translated: "))
+    
+    new_code = ""
+    print(new_code)
+cypher()
+
+
+
+
+
+
+
