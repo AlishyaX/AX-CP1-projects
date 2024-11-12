e488fa15c1c2511941518ae100b3a38976e45800
AlishyaX <alishya.xavier@ucas-edu.net>
2024-11-11 19:33:46 -0700

    Made my secret cypher

--- Functions/ProficiencyTest.py
+++ Functions/ProficiencyTest.py
@@ -1,12 +1,14 @@
 #Alishya Xavier, ProficiencyTest: Secret Cypher
 
 def cypher():
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
     new_alphabet = ""
     code = str(input("Type in the code that you want translated: "))
-    
-    new_code = ""
-    print(new_code)
+    for char in code:
+        char = ord(char)+7
+        new_alphabet = new_alphabet+(chr(char))
+    print("original code:",code)
+    print("cyphered code:", new_alphabet)
 cypher()
 
 
