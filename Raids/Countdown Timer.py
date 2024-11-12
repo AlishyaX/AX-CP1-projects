4f1ea06ee9fe04bbcda58fcc0a35be165c39b8e3
AlishyaX <alishya.xavier@ucas-edu.net>
2024-11-12 10:50:16 -0700

    strated raid

---
+++ Raids/Countdown Timer.py
@@ -0,0 +1,10 @@
+#Alishya Xavier, Raid: Countdown Timer
+user = float(input('How long do you want your timer to run: '))
+while True:
+    print(user)
+    user-=1
+    if user < 0:
+        break
+    else:
+        continue
+
