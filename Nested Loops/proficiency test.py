b09239076e3e2255ac9c12a12d124f4f4aff4744
AlishyaX <alishya.xavier@ucas-edu.net>
2024-11-19 10:51:24 -0700

    started proficiency test

--- Nested Loops/proficiency test.py
+++ Nested Loops/proficiency test.py
@@ -1,2 +1,29 @@
-#Alishya Xavier
+#Alishya Xavier, TicTacToe project
+
+board = [[' ', ' ', ' '],
+         [' ', ' ', ' '],
+         [' ', ' ', ' ']]
+
+def print_board():
+    for row in board:
+        print('|'.join(row))
+        print('-----')
+
+def check_win(player):
+    #This checks each of the rows
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+    
+    #This checks each of the columns
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+            return True
+    
+    #This checks each of the diagnols
+    if all(board[i][i] == player for i in range(3)):
+        return True
+    if all(board[i][i-2] == player for i in range(3)):
+        return True
+
 
