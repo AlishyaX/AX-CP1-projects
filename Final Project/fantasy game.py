95f979b7ae2ad2f04bc6b99f4e48f0117ac68fdd
AlishyaX <alishya.xavier@ucas-edu.net>
2024-12-17 15:20:36 -0700

    made final

--- Final Project/fantasy game.py
+++ Final Project/fantasy game.py
@@ -11,6 +11,7 @@ medals = 0
 #This function I run evertime to see if the main_char has won the game yet or lost the game yet
 def conclude():
     if medals == 4 and life > 0 and days >=0 and health >=0:
+        print('After you completed the last battle, you found this underground passage way \nand rescued everyone that was trapped down there by the mythical creatures.')
         print('You have completed the mission')
         time.sleep(4)
         print('Congradulations! Your name will be forever remembered in the legacy!')
@@ -45,15 +46,15 @@ def haunted_barn():
     elif ectoplasm == '2':
         print('The ectoplasm couldn\'t find you and decided to leave')
         print('As you were leaving, you found a dusty old box.')
-        print('You open the box and find a key that had the letters written d r a g o n carved into it. ')
-        key = input('Do you want to put the key in your backpack(y/n):  ')
-        if key == 'y':
-            items.append('key')
+        print('You open the box and find a flashlight that had the words "It\'s a scary world out there')
+        f = input('Do you want to put the flashlight in your backpack(y/n):  ')
+        if f == 'y':
+            items.append('flashlight')
             print('You then left the barn.')
             print('Here are all of the items you have in your backpack:')
             print(items)
             
-        elif key == 'n':
+        elif f == 'n':
             print('You leave the barn.')
             
         else:
@@ -127,6 +128,7 @@ def villages():
     time.sleep(2)
     print(main_char,': I can\'t believe all of that happened but don\'t worry I will figure out how to fix it')
     time.sleep(2)
+    print('Before you left the old man gave you ')
     days-=.5
     print('You now have',health,'health points and',days,'days left.')
     conclude()
@@ -143,7 +145,7 @@ def riddles_barn():
     time.sleep(5)
     print('If you figure out what it is then you will be able to open the safe.')
     for i in range(9):
-        safe = input('What is the answer: ').lower()
+        safe = input('If you want to leave the game type leave\nWhat is the answer: ').lower()
         if safe == 'book' or safe == 'a book':
             print('That is correct')
             print('When you opened the safe you found this purple potion with a note that says use wisely!')
@@ -161,6 +163,8 @@ def riddles_barn():
             else:
                 print('You left the riddles barn')
                 conclude()
+        elif safe == 'leave':
+            break
         else:
             print('That is incorrect')
             print('This is your ',i+1,' attempt and you only have 9 attempts before it completly locks you out')
@@ -272,13 +276,215 @@ def dragon_cave():
     else:
         conclude()
 
-def witches_castle():
-    print('')
-
-def forest():
 
 def tarton_sea():
+    global life, days, medals
+    days -=1
+    print('Here are all of the items you have in your backpack:')
+    print(items)
+    if 'conch' in items:
+        print('You have the required item that can be used during battle.')
+    else:
+        print('You don\'t have the required item.' )
+    m = input('Do you want to start the battle(y/n):')
+    if m == 'y':
+        print('As you were walking around you heard this beautiful sound coming from the tarton sea.')
+        time.sleep(5)
+        print('The sound almost felt like it was pulling you in as you started mindlessly walking towards the sound.')
+        time.sleep(7)
+        print('As you were about to walk onto the sand beach the sound stopped and you suddenly collapsed in front of a bush.')
+        time.sleep(5)
+    else:
+        conclude()
+    print('As you slowly gained conscious you peeped over the bush to see a group of boys mindlessly walking forward.')
+    time.sleep(5)
+    print('When you looked at what they were walking towards, you remembered learning about them?')
+    time.sleep(4)
+    print('It looked like a mermaid of some sort, Oh wait you remember it is a siren.')
+    time.sleep(7)
+    siren_d = input('We have to do something to save the boys!\nWhich one do you want to do:\n1. Get an item out of your backpack\n2. Quickly get up and run at the siren to scare it off\n3. Go call for help to see if someone can help ')
+    if siren_d =='1':
+        print('Here are the items in your backpack',items)
+        s_d = input('Which item do you wat to take out of your backpack: ')
+        if s_d == 'conch':
+            print('You took the conch and remembering all of the information given to you while training started to blow on one side.')
+        else:
+            print('You tried to use that item but then got caught by the siren and the siren controlled your mind.')
+            time.sleep(6)
+            life -=1
+            print('You lost one of your lifes and have been respawned.')
+            print('You now have',life,' lives left.')
+            conclude()
+        time.sleep(7)
+        print('Suddenly the boys broke out of the spell and started to run away.')
+        time.sleep(4)
+        print('Then the siren started getting light headed a bit so you blew harder')
+        time.sleep(6)
+        print('The siren\'s mind was spinning in circles as it collapsed on the floor, becoming unconscious forever.')
+        medals+=1
+        print('You have recieved a medal and you only need 4 to complete the mission.')
+        print('You now have',medals,'medals.')
+        conclude()
+    elif siren_d == '2':
+        print('While you were charging at the siren it suddenly saw you and managed to control your mind.')
+        time.sleep(5)
+        print('The siren then took control of you.')
+        time.sleep(6)
+        life -=1
+        print('You lost one of your lifes and have been respawned.')
+        print('You now have',life,' lives left.')
+        conclude()
+    elif siren_d == '3':
+        print('As you were getting up to run away, the siren spotted you and was able to control your mind.')
+        time.sleep(5)
+        print('The siren then took control of you.')
+        time.sleep(6)
+        life -=1
+        print('You lost one of your lifes and have been respawned.')
+        print('You now have',life,' lives left.')
+        conclude()
+    else:
+        print('That is not one of the options.')
+        tarton_sea()
+    
 
+def forest():
+    global life, days, medals
+    days -=1
+    print('Here are all of the items you have in your backpack:')
+    print(items)
+    if 'flashlight' in items:
+        print('You have the required item that can be used during battle.')
+    else:
+        print('You don\'t have the required item.' )
+    v_b = input('Do you want to start the battle(y/n):')
+    if v_b == 'y':
+        print('You were walking behind the villages when you heard some wierd sounds coming from the forest.')
+        time.sleep(6)
+        print('And then suddenly you heart these screams also coming from there.')
+        time.sleep(7)
+        print('You decide to go try to help so you start to run through the forest towards where you heard the sound coming from.')
+        time.sleep(5)
+        print('You then stop right in front of a tree, and as you peeked to the other side you saw this VAMPIRES!')
+        time.sleep(10)
+        print('There were also a bunch of kids crying on the ground being surrounded by them.')
+        v = input('What do you do:\n1. Run out into the group and scare them off\n2. Make a distraction by throwing some sticks to the opposite side\n3. Go get help\n 4.Get something out of your backpack')
+        if v == '1':
+            print('The vampires all charged at you and caught you.')
+            time.sleep(6)
+            life -=1
+            print('You lost one of your lifes and have been respawned.')
+            print('You now have',life,' lives left.')
+            conclude()
+        elif v == '2':
+            print('The vampires could smell a human and after hearing the sound coming from there, they left the kids tied and ran over there.')
+            time.sleep(7)
+            print('You then wernt and untied the children, letting them go back home quickly.')
+            time.sleep(6)
+            print('Right after the children left you heard the vampires coming back!')
+            d = input('What do you do:\n1. Run\n2. Get something out of your backpack\n3. Hide')
+            if d == 2:
+                print('Here are the items in your backpack',items)
+                s_d = input('Which item do you want to take out of your backpack: ')
+                if s_d == 'flashlight':
+                    print('You took the flashlight and when they came to you, you put it on the highest brightness.')
+                    time.sleep(5)
+                    print('They started getting frightened as you then chased them around.')
+                    time.sleep(7)
+                    print('Suddenly the vampires got stuck in some quick sand and were never saw again.')
+                    medals+=1
+                    print('You have recieved a medal and you only need 4 to complete the mission.')
+                    print('You now have',medals,'medals.')
+                    conclude()
+                else:
+                    print('You tried to use that item but then got caught by the vampires, leaving them a feast.')
+                    time.sleep(6)
+                    life -=1
+                    print('You lost one of your lifes and have been respawned.')
+                    print('You now have',life,' lives left.')
+                    conclude()
+
+            else:
+                print('While you were doing that, the vampires found you and had a feast.')
+                time.sleep(6)
+                life -=1
+                print('You lost one of your lifes and have been respawned.')
+                print('You now have',life,' lives left.')
+                conclude()
+        else:
+            print('As you were about to do that, the vampires saw you and charged at you, ending up catching you.')
+            time.sleep(6)
+            life -=1
+            print('You lost one of your lifes and have been respawned.')
+            print('You now have',life,' lives left.')
+            conclude()
+    else:
+        conclude()
+
+
+def witches_castle():
+    global life, days, medals
+    days -=1
+    print('Here are all of the items you have in your backpack:')
+    print(items)
+    if 'potion' in items:
+        print('You have the required item that can be used during battle.')
+    else:
+        print('You don\'t have the required item.' )
+    w_b = input('Do you want to start the battle(y/n):')
+    if w_b == 'y':
+        print('As you were walking around past the villages you saw this huge castle out completly in the middle of nowhere.')
+    else:
+        conclude()
+    time.sleep(5)
+    print('You decided to walk through the forest at night since you can\'t waste too much time.')
+    time.sleep(5)
+    print('As you were walking you heard a lot of wherewolves howling at the full moon.')
+    time.sleep(4)
+    print('When you finally got out of the trees, the huge dark castle was waiting ahead. ')
+    time.sleep(7)
+    print('You then decided to sneak up to the castle and go through the back door.')
+    time.sleep(6)
+    print('As you were walking up the stairs you heard cackling coming from the kitchen area.')
+    time.sleep(7)
+    print('You then ended up peeping the corner to see children trapped in a container ready to be put into a bubbling couldren with green liquid slipping out of it.')
+    time.sleep(8)
+    c = input('We have to do something before it is too late!\n What should we do:\n1. Get something out of your backpack\n2. quickly run in and grab the children\n3. Go attack the witch')
+    if c == '1':
+        print('Here are the items in your backpack',items)
+        s_d = input('Which item do you want to take out of your backpack: ')
+        if s_d == 'potion':
+            print('You took the potion and when the witch left the room for a minute you put the potion in the stew.')
+            time.sleep(5)
+            print('Then when the witch came back and took a sip of her stew, she suddenly went unconscious and never woke up.')
+            time.sleep(6)
+            print('You then took the children and went back to the village and found their parents.')
+            medals+=1
+            print('You have recieved a medal and you only need 4 to complete the mission.')
+            print('You now have',medals,'medals.')           
+            conclude()
+        else:
+            print('You tried to use that item but then the witch caught you and got you ready for the stew.')
+            time.sleep(6)
+            life -=1
+            print('You lost one of your lifes and have been respawned.')
+            print('You now have',life,' lives left.')
+            conclude()            
+
+    elif c == '2':
+        print('As you were running through the kitchen, the witch caught you and got you also ready for the stew.')
+        time.sleep(6)
+        life -=1
+        print('You lost one of your lifes and have been respawned.')
+        print('You now have',life,' lives left.')
+        conclude()
+    elif c == '3':
+        print('You tried to attack the witch but the witch was all ready for you and put a spell on you.\nThen she got you alsp ready for the stew.')
+        time.sleep(6)
+        life -=1
+        print('You lost one of your lifes and have been respawned.')
+        print('You now have',life,' lives left.')
+        conclude()
 
 
 #This is the function that I keep running for the main_char to pick which place they want to go to 
@@ -311,9 +517,15 @@ def choices():
 def intro():
     global main_char
     main_char = input('Welcome to this Fantasy/Adventure game!\nWhat is your name:\n')
-
+    time.sleep(4)
+    print('So far', main_char,' has been training ever since he/she was a kid to be able to complete the hardest challenge of their lifetime. ')
+    time.sleep(7)
+    print('If you do complete the mission then you will be praised and have the honor of being an official armender.')
     start_mission = input('Are you ready to start your mission: (y/n) ')
     if start_mission == 'y':
+        print('For your mission you will be sent to Aruna islands in a private jet.')
+        time.sleep(6)
+        print('It seems that people have been going missing and at this rate everyone on that island will be gone\nin 7 days. ')
         conclude()
     else:
         breakpoint
